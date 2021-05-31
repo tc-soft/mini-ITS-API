ad53ad43321df365a27de6289ed5c824f2b8819a
Tadeusz Ciszewski <office@tc-soft.pl>
2021-05-31 17:32:49 +0200

    feat: Add dotenv library to loads environment variables

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT; 
 
 app.get('/', (req, res) => {
     console.log('[TEST]!');
