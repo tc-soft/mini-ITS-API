e6fe4365b337aef974c332a253f5dd8180562fe6
Tadeusz Ciszewski <office@tc-soft.pl>
2021-06-01 22:13:58 +0200

    feat: Add JSON body parser

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT; 
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     console.log('[TEST]!');
     res.send('Hello from Homepage'); 
